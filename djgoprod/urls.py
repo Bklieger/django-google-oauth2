396ceb3ff2768adc967702b872dd309f9ee62006
Benjamin Klieger <62450410+Bklieger@users.noreply.github.com>
2024-06-11 09:30:25 -0700

    Update urls.py with option to disable non-Google OAuth allauth endpoints

--- djgoprod/urls.py
+++ djgoprod/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')), # Include accounts app urls
     path('admin/', admin.site.urls), # Include admin urls
     path('allauth/', include('allauth.urls')), # Include allauth urls
+    # path('allauth/', include('allauth.socialaccount.providers.google.urls')), # You can use this instead of include('allauth.urls') to only enable Google OAuth allauth endpoints.
 ]
